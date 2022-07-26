6ec8608fae5c2594872d85de61454a9c2a8a9cd6
varNicolas <nicolas.oliveir321@gmail.com>
2022-07-25 22:07:03 -0300

    commit pattern

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let time = 5000, 
+cuurrentIndex = 0, 
+images = document.querySelectorAll(".containerProjects img")
+max = images.length;
+
+nextImage = ()=>{
+  images[cuurrentIndex]
+  .classList.remove('selected');
+
+  cuurrentIndex++
+  if(cuurrentIndex == max )
+    cuurrentIndex = 0;
+  
+  images[cuurrentIndex].classList.add('selected');
+  }
+start = ()=>{
+setInterval(() => {
+  // Troca de imagem
+  nextImage();
+}, time);
+}
+window.addEventListener('load', start);
